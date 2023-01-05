d7c5fbf0a2f2e8bef6dc2a97ff0b1648578687ea
Matthew <94242888+mvtran562@users.noreply.github.com>
2023-01-04 23:40:11 -0800

    Delete ParseSite directory

--- ParseSite/Boolean Test.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-fp = urllib.request.urlopen("https://www.newbalance.com/pd/550/BB550V1-36007.html")
-mybytes = fp.read()
-
-mystr = mybytes.decode("utf8")
-fp.close()
-
-print(mystr)
